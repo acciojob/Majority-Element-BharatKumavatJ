549ef494a0497a3da793235cb36a57d4af730b8b
BharatKumavatJ <98304786+BharatKumavatJ@users.noreply.github.com>
2022-07-08 19:00:40 +0530

    Add files via upload

--- solution.js
+++ solution.js
@@ -1,6 +1,20 @@
 // arr = array of numbers
-function major(arr)
+function major(nums)
 {
   //your code here
+  var ele;
+  var maj = 0;
+  for(var i in nums){
+      if(maj == 0){
+          ele = nums[i];
+
+      }
+      if(ele == nums[i]){
+          maj++;
+      }else{
+          maj--;
+      }
+  }
+  return ele;
 }
 module.exports=major;
